ebabd80478682a47a752d46fb1e3423b09db258e
Smarajit Das <sdas9696969@gmail.com>
2020-12-26 18:33:44 +0530

    Update index.js

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,2 +1,6 @@
 const fs = require('fs');
 fs.copyFileSync('file1.txt', 'file2.txt');
+const temp = require('superheroes');
+console.log(temp.random());
+var temp1 = require('supervillains');
+console.log(temp1.random());
