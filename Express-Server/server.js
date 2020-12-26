c4d855d6c89e2c78e0c14bf879dbb5f10bfdb742
Smarajit Das <sdas9696969@gmail.com>
2020-12-26 22:10:38 +0530

    Update server.js

--- Express-Server/server.js
+++ Express-Server/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+app.get('/', function (req, res) {
+    res.send('<h1>Hello</h1>');
+});
+app.get('/contact', function (req, res) {
+    res.send('<h1>Contact Page</h1>');
+});
+app.get('/about', function (req, res) {
+    res.send('<h1>Smarajit Das</h1>');
+});
 app.listen(2000, function () {
     console.log('Server Started on Port 2000');
 });
