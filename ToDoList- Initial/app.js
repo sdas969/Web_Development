25275ffa5be5205dfad997b0c1e2534e04bd1ebb
Smarajit Das <sdas9696969@gmail.com>
2020-12-30 01:39:15 +0530

    Update app.js

--- ToDoList- Initial/app.js
+++ ToDoList- Initial/app.js
@@ -4,10 +4,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0)
-        res.sendFile(__dirname + '/index.html');
-    else
-        res.sendFile(__dirname + '/index.html');
+    res.render('list', { foo: today.getDate() });
 });
 
 app.listen(process.env.PORT || 2000, function () {
