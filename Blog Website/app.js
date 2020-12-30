6f0432d215c33ee954c166c1da5eb5bb267eed0b
Smarajit Das <sdas9696969@gmail.com>
2020-12-31 04:47:51 +0530

    Update app.js

--- Blog Website/app.js
+++ Blog Website/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const lowerCase = require('lodash');
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -31,14 +32,18 @@ app.get('/contact', function (req, res) {
 app.get('/compose', function (req, res) {
   res.render('compose');
 });
+
+
 app.get('/posts/:postName', function (req, res) {
-  console.log(req.params.postName);
+  let reqPage = lowerCase.lowerCase(req.params.postName);
   posts.forEach(function (post) {
-    if (post.title === req.params.postName) {
+    if (lowerCase.lowerCase(post.title) === reqPage) {
       console.log('Match Found');
     }
   });
 });
+
+
 app.post('/compose', function (req, res) {
   let post = {
     title: req.body.title,
