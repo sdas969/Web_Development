854725b8d51c386788fa291fa664d6eb64c77182
Smarajit Das <sdas9696969@gmail.com>
2020-12-30 12:18:08 +0530

    Update app.js

--- ToDoList- Initial/app.js
+++ ToDoList- Initial/app.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+var items = [];
+
 app.get('/', function (req, res) {
     var today = new Date();
-    var day = today.getDay();
-    var ans = '';
-    switch (day) {
-        case 0: ans = 'Sunday'; break;
-        case 1: ans = 'Monday'; break;
-        case 2: ans = 'Tuesday'; break;
-        case 3: ans = 'Wednesday'; break;
-        case 4: ans = 'Thursday'; break;
-        case 5: ans = 'Friday'; break;
-        case 6: ans = 'Saturday'; break;
-        default: ans = ''; break;
-    }
-    res.render('list', { dayname: ans, day: day });
+    var day = today.toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'long' });
+    res.render('list', { dayname: day, items: items });
+});
+
+app.post('/', function (req, res) {
+    items.push(req.body.newitem);
+    console.log(items);
+    res.redirect('/');
 });
 
 app.listen(process.env.PORT || 2000, function () {
