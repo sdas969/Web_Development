fe964e7b9b93f749cf00566ddd1d8ca3bd22930c
Smarajit Das <sdas9696969@gmail.com>
2021-01-02 04:38:45 +0530

    Update app.js

--- Fruits Project/app.js
+++ Fruits Project/app.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true, 
 const fruitSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Please check your data entry'],
     },
     rating: {
         type: Number,
@@ -55,25 +55,79 @@ Fruit.insertMany([
 const personSchema = new mongoose.Schema({
     name: String,
     age: Number,
+    favouriteFruit: fruitSchema,
 });
+
+const favFruit = new Fruit(
+    {
+        name: 'Niceee1',
+        rating: 10,
+        review: 'Hello',
+    }
+);
+
+favFruit.save();
+
+const favFruit1 = new Fruit({
+    name: 'niceee1',
+    rating: 10,
+    review: 'Hello1',
+});
+favFruit1.save();
 
 const Person = mongoose.model('person', personSchema);
 
+const favPerson = new Person({
+    name: 'Niceeeeeeeeeeeee',
+    age: 21,
+    favouriteFruit: favFruit,
+});
+
+Person.updateOne({ _id: '5fef8f2284d67e4cccf0d426' }, { favouriteFruit: favFruit1 }, function (err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('Updated');
+});
+
+favPerson.save();
 const person = new Person({
-    name: 'Smarajit',
-    age: 21
+    name: 'Smara',
+    age: 21,
 });
 
 person.save();
 
+Fruit.updateOne({ _id: '5fef883600e43a626c67cc66' }, { name: 'niceeee' }, function (err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('Updated Successfully');
 
+});
 Fruit.find(function (err, fruits) {
     if (err)
         console.log(err);
     else {
         fruits.forEach(function (fruit) {
             console.log(fruit.name);
+
         });
         mongoose.connection.close();
     }
+});
+
+
+Fruit.deleteOne({ name: 'niceeee' }, function (err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('Deleted Successfully');
+});
+
+Person.deleteMany({ name: 'Smarajit' }, function (err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('Deleted successfully');
 });
