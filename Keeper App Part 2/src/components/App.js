79396481d9098149ddb12fe74d59fb1968434f0b
Smarajit Das <sdas9696969@gmail.com>
2021-01-06 02:05:56 +0530

    Update

--- Keeper App Part 2/src/components/App.js
+++ Keeper App Part 2/src/components/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from '../notes.js';
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note => <Note title={note.title} content={note.content} />)}
       <Footer />
     </div>
   );
