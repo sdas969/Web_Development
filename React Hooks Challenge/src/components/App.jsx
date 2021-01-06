6b93643979c879202a140518165577ed57d31c05
Smarajit Das <sdas9696969@gmail.com>
2021-01-06 13:48:03 +0530

    Update

--- React Hooks Challenge/src/components/App.jsx
+++ React Hooks Challenge/src/components/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function App() {
+  let initialTime = new Date().toLocaleTimeString();
+  const [time, update] = React.useState(initialTime);
+  function getTime() {
+    let updateTime = new Date().toLocaleTimeString();
+    update(updateTime);
+  }
+  setInterval(getTime, 1000);
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
