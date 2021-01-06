2e3504cb0d169d802336f4d80a487ed2a812511e
Smarajit Das <sdas9696969@gmail.com>
2021-01-06 15:32:02 +0530

    Update

--- React Changing Complex State/src/components/App.jsx
+++ React Changing Complex State/src/components/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  let [fname, updateFName] = useState('');
+  let [lname, updateLName] = useState('');
+
+  function handlefChange(event) {
+    fname = event.target.value;
+  }
+  function handlelChange(event) {
+    lname = event.target.value;
+  }
+  function handleClick(event) {
+    updateFName(fname);
+    updateLName(lname);
+    event.preventDefault();
+  }
+
+
+
   return (
     <div className="container">
       <h1>Hello {fname + ' ' + lname}</h1>
       <form>
-        <input onChange={handleChange} name="fName" placeholder="First Name" />
-        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handlefChange} name="fName" placeholder="First Name" />
+        <input onChange={handlelChange} name="lName" placeholder="Last Name" />
         <button onClick={handleClick}>Submit</button>
       </form>
     </div>
