643dae063495f37592b0669054c57740be0bceda
Smarajit Das <sdas9696969@gmail.com>
2021-01-06 15:52:00 +0530

    Update

--- React Changing Complex State/src/index.js
+++ React Changing Complex State/src/index.js
@@ -3,3 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+let temp = ['hello', 'hello1', 'hello2'];
+let temp1 = ['nice', 'nice1', 'nice2', ...temp];
+console.log(temp1);
+
+const fullName = {
+    fname: 'Smarajit',
+    lname: 'Das'
+}
+
+const user = {
+    ...fullName,
+    id: 1,
+}
+console.log(user.fname);
