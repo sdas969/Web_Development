824652f7f2a3e48b27e0a25b916f36d9e21120ad
Smarajit Das <sdas9696969@gmail.com>
2021-01-06 12:51:13 +0530

    Update

--- React Hooks/src/components/App.jsx
+++ React Hooks/src/components/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function App() {
-  return <div />;
+
+  const [count, updateCountto] = React.useState(0)
+
+  function increase() {
+    updateCountto(count + 1);
+  }
+  function decrease() {
+    updateCountto(count - 1);
+  }
+
+
+  return <div className="container">
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
+    <button onClick={decrease}>-</button>
+  </div>;
 }
 
 export default App;
