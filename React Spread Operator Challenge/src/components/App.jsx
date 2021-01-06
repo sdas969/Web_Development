7f9e89a7f25b7da5a426cbab6285d3f695310bc3
Smarajit Das <sdas9696969@gmail.com>
2021-01-07 01:20:04 +0530

    Update App.jsx

--- React Spread Operator Challenge/src/components/App.jsx
+++ React Spread Operator Challenge/src/components/App.jsx
@@ -1,20 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import ToDoItem from './ToDoItem';
 function App() {
+
+  let [listItems, updateListItems] = useState([]);
+  let [temp, updateText] = useState('');
+  function handleChange(event) {
+    temp = event.target.value;
+    updateText(temp);
+  }
+  function handleClick() {
+    if (temp)
+      listItems.push(temp);
+    updateListItems([...listItems]);
+    temp = '';
+    updateText(temp);
+  }
+  function itemClick(id) {
+    listItems = listItems.filter((name, index) => index !== id);
+    updateListItems([...listItems]);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={temp} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {listItems.map((element, index) => <ToDoItem key={index} id={index} itemClick={itemClick} itemName={element} />)}
         </ul>
       </div>
     </div>
