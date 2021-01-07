ca0d3e50b143af104c3261e23c9ac397ca390640
Smarajit Das <sdas9696969@gmail.com>
2021-01-07 16:01:21 +0530

    Update

---
+++ website-project/src/App.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
+import Button from '@material-ui/core/Button';
+import Buton from './Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+export default function BackToTop(props) {
+  return (
+    <header>
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar >
+          <Toolbar>
+            <img src="http://mnit.ac.in/Images/logo.png" />
+            <div>
+              <h3>Malaviya National Institute of Technology, Jaipur</h3>
+              <h3>मालवीय राष्ट्रीय प्रौद्योगिकी संस्थान जयपुर</h3>
+            </div>
+            <div>
+              <Button variant="contained" color="primary" href="#contained-buttons" disableElevation>
+                About Us
+            </Button>
+            </div>
+            <div>
+              <Button variant="contained" color="primary" href="#contained-buttons" disableElevation>
+                Administration
+            </Button>
+            </div>
+            <div>
+              <Button variant="contained" color="primary" href="#contained-buttons" disableElevation>
+                Central Facilities
+            </Button>
+            </div>
+            <div>
+              <Button variant="contained" color="primary" href="#contained-buttons" disableElevation>
+                Prospective Students
+            </Button>
+            </div>
+            <div>
+              <Button variant="contained" color="primary" href="#contained-buttons" disableElevation>
+                Departments
+            </Button>
+            </div>
+
+
+          </Toolbar>
+        </AppBar>
+        <Toolbar id="back-to-top-anchor" />
+        <Container>
+          <Box my={2}>
+            {[...new Array(12)]
+              .map(
+                () => [`Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`, `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`, `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`]
+              )
+              .join('\n')}
+          </Box>
+        </Container>
+        <ScrollTop {...props}>
+          <Fab color="secondary" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
+      </React.Fragment >
+    </header>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
